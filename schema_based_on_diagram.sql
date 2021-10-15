5a85229c73ae922016b892b147d4e54851b70ae2
onesis <Onesis.olea@gmail.com>
2021-10-15 13:26:04 -0400

    CREATE TABLE invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -30,3 +30,15 @@ CREATE TABLE medical_histories  (
     FOREIGN KEY (patient_id) REFERENCES patients(id),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE invoice_items(
+    id               INT GENERATED ALWAYS AS IDENTITY,
+    unit_price       DECIMAL,  
+    quantity         INT,
+    total_price      DECIMAL, 
+    invoice_id       INT,    
+    treatment_id     INT,            
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id),    
+    PRIMARY KEY (id)
+);
