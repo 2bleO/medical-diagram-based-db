0efa9d0e3b84c4d12999b50195ae6aa70dc71bd3
onesis <Onesis.olea@gmail.com>
2021-10-15 13:24:17 -0400

    create treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,10 @@ CREATE TABLE patients (
     date_of_birth   DATE,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE  treatments (
+    id              INT GENERATED ALWAYS AS IDENTITY,
+    type            VARCHAR(100),
+    name            VARCHAR(100),
+    PRIMARY KEY(id)
+);
